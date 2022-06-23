7baa025528de5928d88998ddd52646babcb46607
KirillKorotkiy <104353705+KirillKorotkiy@users.noreply.github.com>
2022-06-23 10:03:57 +0300

    1

--- module-02/js/task-06.js
+++ module-02/js/task-06.js
@@ -5,17 +5,23 @@ let total = 0;
 
 while (input !== null) {
   input = prompt("Введите число");
-  let inputNumber= Number(input);
-  numbers.push(inputNumber);
+  const turnNumber = Number(input);
   if (input === null) {
     break;
-  }
-}
+  } 
+  if(!Number.isNaN(turnNumber)){
+    numbers.push(turnNumber)}
+   else { 
+    alert('Было введено не число, попробуйте еще раз')
+}}
+
+
 
 if(numbers.length !==0){
   for(let i =0; i<numbers.length; i+=1){
     total +=numbers[i];
   }
 }
+console.log(numbers)
 console.log(`Общая сумма чисел равна ${total}`)
 
