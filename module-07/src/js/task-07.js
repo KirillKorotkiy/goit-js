09d04a8922744da3f641861048cbc21d863fe6c5
KirillKorotkiy <104353705+KirillKorotkiy@users.noreply.github.com>
2022-07-28 14:22:44 +0300

    Delete module-07 directory

--- module-07/src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputRef = document.querySelector('#font-size-control');
-const spanRef = document.querySelector('#text');
-console.dir(spanRef)
-
-inputRef.addEventListener('input', event =>{
-    spanRef.style.fontSize = event.currentTarget.value + "px";
-})
